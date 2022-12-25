0c76f9f320b47286beae4e98420489bbcf57750c
lahlaliwassim2 <wlahlali34@gmail.com>
2022-12-25 11:49:13 +0100

    ajouter une image via l objet Vue en utulisant 'v-bind:' pour que l html connait

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ new Vue ({
     el:'#app',
     data : {
         message: "hello hello",
-        
+        image: "https://images.pexels.com/photos/14840714/pexels-photo-14840714.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
     },
     methods : {
         saySalam: function() {
