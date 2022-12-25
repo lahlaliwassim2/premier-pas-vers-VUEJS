7a847e9f5a57de38a6e6271e737e67c33e5b7f55
lahlaliwassim2 <wlahlali34@gmail.com>
2022-12-25 11:38:54 +0100

    integrer une methode

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 new Vue ({
     el:'#app',
     data : {
-        message: "hello hello"
+        message: "hello hello",
+        
+    },
+    methods : {
+        saySalam: function() {
+            return `hello frinds I'm Wassim Lahlali `
+        }
     }
 })
