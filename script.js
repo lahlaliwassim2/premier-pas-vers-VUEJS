95720ef4f55f46bc11618afbfdd80a3027fbebc7
lahlaliwassim2 <wlahlali34@gmail.com>
2022-12-25 11:53:10 +0100

    ajouter la valeur de l input depuis l objet Vue en utulisant juste ':' à la place de 'v-bind' grace a l amise a jour de Vuejs 2 dans la DATA BINDING

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ new Vue ({
     el:'#app',
     data : {
         message: "hello hello",
+        input: "valeure de l input",
         image: "https://images.pexels.com/photos/14840714/pexels-photo-14840714.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
     },
     methods : {
